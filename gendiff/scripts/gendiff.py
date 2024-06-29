9aa5f91f0be1c609a547dae6c76f3da93b15f2d9
ShchenevS <shchenevs@mail.ru>
2024-06-29 23:50:48 +0500

    first step

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(usage='gendiff [-h] first_file second_file',
+                                     description='Compares two configuration files and shows a difference.')
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    args = parser.parse_args()
+    
+
+if __name__ == '__main__':
+    main()
