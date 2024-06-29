cb3f0034208006482ee8186fd469e7cf6f70f904
ShchenevS <shchenevs@mail.ru>
2024-06-29 23:59:10 +0500

    second step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,8 +3,9 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(usage='gendiff [-h] first_file second_file',
+    parser = argparse.ArgumentParser(usage='gendiff [-h] [-f FORMAT] first_file second_file',
                                      description='Compares two configuration files and shows a difference.')
+    parser.add_argument("-f", "--format", dest="FORMAT", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
     args = parser.parse_args()
