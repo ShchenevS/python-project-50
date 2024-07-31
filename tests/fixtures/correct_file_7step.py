f63ff93c092840cbde62c6f8f5efe88df5b903f1
ShchenevS <shchenevs@mail.ru>
2024-07-31 22:14:30 +0500

    fix lint warnings

--- tests/fixtures/correct_file_7step.py
+++ tests/fixtures/correct_file_7step.py
@@ -1,11 +1,12 @@
-result = f"""Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]"""
+result = "Property 'common.follow' was added with value: false\n"
+result += "Property 'common.setting2' was removed\n"
+result += "Property 'common.setting3' was updated. From true to null\n"
+result += "Property 'common.setting4' was added with value: 'blah blah'\n"
+result += "Property 'common.setting5' was added with value: [complex value]\n"
+result += "Property 'common.setting6.doge.wow'"
+result += " was updated. From '' to 'so much'\n"
+result += "Property 'common.setting6.ops' was added with value: 'vops'\n"
+result += "Property 'group1.baz' was updated. From 'bas' to 'bars'\n"
+result += "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
+result += "Property 'group2' was removed\n"
+result += "Property 'group3' was added with value: [complex value]"
