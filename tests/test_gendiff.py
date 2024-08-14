c94d1632d645d5e9792dc8483c51c19b0abeaecb
ShchenevS <shchenevs@mail.ru>
2024-08-14 23:02:04 +0500

    fix test_gendiff by using parametrize

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,3 +1,4 @@
+import pytest
 from gendiff.modules.gendiff import generate_diff
 from tests.fixtures.correct_file_3n5step import result as result_3n5step
 from tests.fixtures.correct_file_6step import result as result_6step
@@ -5,71 +6,34 @@ from tests.fixtures.correct_file_7step import result as result_7step
 from tests.fixtures.correct_file_8step import result as result_8step
 
 
-def test_generate_diff_no_change():
-    file1_path = './tests/fixtures/file1.json'
-    file2_path = './tests/fixtures/file2.json'
-    correct_file = "{\n    host: hexlet.io\n}"
-    assert generate_diff(file1_path, file2_path) == correct_file
+f1_json = './tests/fixtures/file1.json'
+f2_json = './tests/fixtures/file2.json'
+f3_json = './tests/fixtures/file3.json'
+f4_json = './tests/fixtures/file4.json'
+f5_json = './tests/fixtures/file5.json'
+f1_3s_json = './tests/fixtures/file1_3step.json'
+f2_3s_json = './tests/fixtures/file2_3step.json'
+f1_5s_yaml = './tests/fixtures/file1_5step.yaml'
+f2_5s_yaml = './tests/fixtures/file2_5step.yml'
+f1_6s_json = './tests/fixtures/file1_6step.json'
+f2_6s_json = './tests/fixtures/file2_6step.json'
+f1_6s_yaml = './tests/fixtures/file1_6step.yml'
+f2_6s_yaml = './tests/fixtures/file2_6step.yaml'
+testdata = [
+    (f1_json, f2_json, "{\n    host: hexlet.io\n}", "stylish"),
+    (f1_json, f3_json, "{\n  - host: hexlet.io\n}", "stylish"),
+    (f1_json, f4_json, "{\n    host: hexlet.io\n  + timeout: 50\n}", "stylish"),
+    (f1_json, f5_json, "{\n  - host: hexlet.io\n  + host: hexlet1.io\n}", "stylish"),
+    (f1_3s_json, f2_3s_json, result_3n5step, "stylish"),
+    (f1_5s_yaml, f2_5s_yaml, result_3n5step, "stylish"),
+    (f1_6s_json, f2_6s_json, result_6step, "stylish"),
+    (f1_6s_yaml, f2_6s_yaml, result_6step, "stylish"),
+    (f1_6s_json, f2_6s_json, result_7step, "plain"),
+    (f1_6s_json, f2_6s_json, result_8step, "json")
+]
 
 
-def test_generate_diff_delete():
-    file1_path = './tests/fixtures/file1.json'
-    file3_path = './tests/fixtures/file3.json'
-    correct_file = "{\n  - host: hexlet.io\n}"
-    assert generate_diff(file1_path, file3_path) == correct_file
-
-
-def test_generate_diff_add():
-    file1_path = './tests/fixtures/file1.json'
-    file4_path = './tests/fixtures/file4.json'
-    correct_file = "{\n    host: hexlet.io\n  + timeout: 50\n}"
-    assert generate_diff(file1_path, file4_path) == correct_file
-
-
-def test_generate_diff_change():
-    file1_path = './tests/fixtures/file1.json'
-    file5_path = './tests/fixtures/file5.json'
-    correct_file = "{\n  - host: hexlet.io\n  + host: hexlet1.io\n}"
-    assert generate_diff(file1_path, file5_path) == correct_file
-
-
-def test_gendiff_3step_complex_json_files():
-    file1_path = './tests/fixtures/file1_3step.json'
-    file2_path = './tests/fixtures/file2_3step.json'
-    correct_file = result_3n5step
-    assert generate_diff(file1_path, file2_path) == correct_file
-
-
-def test_gendiff_5step_complex_yaml_files():
-    file1_path = './tests/fixtures/file1_5step.yaml'
-    file2_path = './tests/fixtures/file2_5step.yml'
-    correct_file = result_3n5step
-    assert generate_diff(file1_path, file2_path) == correct_file
-
-
-def test_gendiff_6step_complex_json_files():
-    file1_path = './tests/fixtures/file1_6step.json'
-    file2_path = './tests/fixtures/file2_6step.json'
-    correct_file = result_6step
-    assert generate_diff(file1_path, file2_path) == correct_file
-
-
-def test_gendiff_6step_complex_yaml_files():
-    file1_path = './tests/fixtures/file1_6step.yml'
-    file2_path = './tests/fixtures/file2_6step.yaml'
-    correct_file = result_6step
-    assert generate_diff(file1_path, file2_path) == correct_file
-
-
-def test_gendiff_7step_complex_json_files():
-    file1_path = './tests/fixtures/file1_6step.json'
-    file2_path = './tests/fixtures/file2_6step.json'
-    correct_file = result_7step
-    assert generate_diff(file1_path, file2_path, 'plain') == correct_file
-
-
-def test_gendiff_8step_complex_json_files():
-    file1_path = './tests/fixtures/file1_6step.json'
-    file2_path = './tests/fixtures/file2_6step.json'
-    correct_file = result_8step
-    assert generate_diff(file1_path, file2_path, 'json') == correct_file
+@pytest.mark.parametrize("link1,link2,expected,format", testdata)
+def test_generate_diff(link1, link2, expected, format):
+    diff = generate_diff(link1, link2, format)
+    assert diff == expected
