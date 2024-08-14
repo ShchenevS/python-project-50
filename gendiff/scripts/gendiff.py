30f97fe7750a2738a50e82d2b0fb5d9d9de2c486
ShchenevS <shchenevs@mail.ru>
2024-08-14 23:36:30 +0500

    rename parser.py to cli.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-from gendiff.modules import parser
+from gendiff.modules import cli
 from gendiff.modules.gendiff import generate_diff
 
 
 def main():
     diff = generate_diff(
-        parser.args.first_file,
-        parser.args.second_file,
-        parser.args.FORMAT)
+        cli.args.first_file,
+        cli.args.second_file,
+        cli.args.FORMAT)
     print(diff)
 
 
